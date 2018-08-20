3a3369022d2e78cdd6f18ce9bfb4564f19c871c3
alanachaval <alanachaval@github.com>
2018-08-20 15:08:35 -0300

    Agregado lectura de parametros

--- Fibo2/src/main/java/ar/edu/untref/dyasc/Program.java
+++ Fibo2/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -4,9 +4,28 @@ public class Program {
 
     public static void main(String[] args) {
 
+        int cantidadDeElementos;
+        boolean horizontal;
+        boolean directa;
+        if (args.length == 2) {
+            if (args[0].length() != 5 || !args[0].substring(0, 3).equals("-o=")
+                    || (!args[0].substring(3, 4).equals("h") && !args[0].substring(3, 4).equals("v"))
+                    || (!args[0].substring(4, 5).equals("d") && !args[0].substring(4, 5).equals("i"))) {
+                System.out.println("Opciones no validas.");
+                return;
+            }
+            horizontal = args[0].substring(3, 4).equals("h");
+            directa = args[0].substring(4, 5).equals("d");
+            cantidadDeElementos = Integer.parseInt(args[1]);
+        } else {
+            cantidadDeElementos = Integer.parseInt(args[0]);
+            horizontal = true;
+            directa = true;
+        }
+
         Fibo fibo = new Fibo();
-        long[] serie = fibo.generarSerie(Integer.parseInt(args[0]));
-        String resultado = fibo.arrayAString(serie, true, true);
+        long[] serie = fibo.generarSerie(cantidadDeElementos);
+        String resultado = fibo.arrayAString(serie, horizontal, directa);
         System.out.println(resultado);
 
     }
