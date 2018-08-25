dd95dcdb6abf68226260436d7ae58dca88f5de02
alanachaval <alanachaval@github.com>
2018-08-25 01:35:36 -0300

    Agregado tests de Direccion

---
+++ Fibo3/src/test/java/ar/edu/untref/dyasc/parser/DireccionTest.java
@@ -0,0 +1,53 @@
+package ar.edu.untref.dyasc.parser;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DireccionTest {
+
+    private Direccion direccion;
+    private Parametros parametros;
+
+    @BeforeEach
+    public void setUpBeforeEach() throws Exception {
+        parametros = new Parametros();
+        direccion = new Direccion();
+    }
+
+    @Test
+    void testHorizontalDirecta() throws OpcionInvalidaException {
+        comparar("hd", true, true);
+    }
+
+    @Test
+    void testConsolaHorizontalInversa() throws OpcionInvalidaException {
+        comparar("hi", true, false);
+    }
+
+    @Test
+    void testConsolaVerticalDirecta() throws OpcionInvalidaException {
+        comparar("vd", false, true);
+    }
+
+    @Test
+    void testConsolaVerticalInversa() throws OpcionInvalidaException {
+        comparar("vi", false, false);
+    }
+
+    @Test
+    void testOpcionInvalida() {
+        assertThrows(OpcionInvalidaException.class, () -> {
+            direccion.GetParametro(parametros, "xy");
+        });
+    }
+
+    private void comparar(String value, boolean horizontal, boolean directa) throws OpcionInvalidaException {
+        direccion.GetParametro(parametros, value);
+        Assert.assertEquals(horizontal, parametros.isHorizontal());
+        Assert.assertEquals(directa, parametros.isDirecta());
+    }
+
+}
