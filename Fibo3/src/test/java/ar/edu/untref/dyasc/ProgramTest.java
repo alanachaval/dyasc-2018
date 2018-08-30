7317c8914589e8499df24a3882ef6e9906439d5f
Alan N. Achaval <achavalalann@gmail.com>
2018-08-30 18:18:19 -0300

    fix reemplazo tabs por espacios

--- Fibo3/src/test/java/ar/edu/untref/dyasc/ProgramTest.java
+++ Fibo3/src/test/java/ar/edu/untref/dyasc/ProgramTest.java
@@ -4,124 +4,124 @@ import org.junit.jupiter.api.Test;
 
 public class ProgramTest {
 
-	// Fibo3
-	@Test
-	void testEjemplovdsalidatxt5() throws Throwable {
-		WritterTest.compararStream("fibo<5> guardado en salida.txt" + FormatterTest.LINE_SEPARATOR, () -> {
-			Program.main(new String[] { "-o=vd", "-f=salida.txt", "5" });
-		});
-		WritterTest.compararArchivo("salida.txt",
-				"fibo<5>:" + FormatterTest.LINE_SEPARATOR + "0" + FormatterTest.LINE_SEPARATOR + "1"
-						+ FormatterTest.LINE_SEPARATOR + "1" + FormatterTest.LINE_SEPARATOR + "2"
-						+ FormatterTest.LINE_SEPARATOR + "3");
-	}
+    // Fibo3
+    @Test
+    void testEjemplovdsalidatxt5() throws Throwable {
+        WritterTest.compararStream("fibo<5> guardado en salida.txt" + FormatterTest.LINE_SEPARATOR, () -> {
+            Program.main(new String[] { "-o=vd", "-f=salida.txt", "5" });
+        });
+        WritterTest.compararArchivo("salida.txt",
+                "fibo<5>:" + FormatterTest.LINE_SEPARATOR + "0" + FormatterTest.LINE_SEPARATOR + "1"
+                        + FormatterTest.LINE_SEPARATOR + "1" + FormatterTest.LINE_SEPARATOR + "2"
+                        + FormatterTest.LINE_SEPARATOR + "3");
+    }
 
-	@Test
-	void testEjemplovds5() throws Throwable {
-		WritterTest.compararStream("fibo<5>: 7" + FormatterTest.LINE_SEPARATOR, () -> {
-			Program.main(new String[] { "-o=hd", "-m=s", "5" });
-		});
-	}
+    @Test
+    void testEjemplovds5() throws Throwable {
+        WritterTest.compararStream("fibo<5>: 7" + FormatterTest.LINE_SEPARATOR, () -> {
+            Program.main(new String[] { "-o=hd", "-m=s", "5" });
+        });
+    }
 
-	@Test
-	void testEjemplovdsalidatxts5() throws Throwable {
-		WritterTest.compararStream("fibo<5> guardado en salida.txt" + FormatterTest.LINE_SEPARATOR, () -> {
-			Program.main(new String[] { "-o=vd", "-f=salida.txt", "-m=s", "5" });
-		});
-		WritterTest.compararArchivo("salida.txt", "fibo<5>:" + FormatterTest.LINE_SEPARATOR + "7");
-	}
+    @Test
+    void testEjemplovdsalidatxts5() throws Throwable {
+        WritterTest.compararStream("fibo<5> guardado en salida.txt" + FormatterTest.LINE_SEPARATOR, () -> {
+            Program.main(new String[] { "-o=vd", "-f=salida.txt", "-m=s", "5" });
+        });
+        WritterTest.compararArchivo("salida.txt", "fibo<5>:" + FormatterTest.LINE_SEPARATOR + "7");
+    }
 
-	// Fibo2
-	@Test
-	void testEjemplovd5() throws Throwable {
-		WritterTest.compararStream("fibo<5>:" + FormatterTest.LINE_SEPARATOR + "0" + FormatterTest.LINE_SEPARATOR + "1"
-				+ FormatterTest.LINE_SEPARATOR + "1" + FormatterTest.LINE_SEPARATOR + "2" + FormatterTest.LINE_SEPARATOR
-				+ "3" + FormatterTest.LINE_SEPARATOR, () -> {
-					Program.main(new String[] { "-o=vd", "5" });
-				});
-	}
+    // Fibo2
+    @Test
+    void testEjemplovd5() throws Throwable {
+        WritterTest.compararStream("fibo<5>:" + FormatterTest.LINE_SEPARATOR + "0" + FormatterTest.LINE_SEPARATOR + "1"
+                + FormatterTest.LINE_SEPARATOR + "1" + FormatterTest.LINE_SEPARATOR + "2" + FormatterTest.LINE_SEPARATOR
+                + "3" + FormatterTest.LINE_SEPARATOR, () -> {
+                    Program.main(new String[] { "-o=vd", "5" });
+                });
+    }
 
-	@Test
-	void testEjemplohi8() throws Throwable {
-		WritterTest.compararStream("fibo<8>: 13 8 5 3 2 1 1 0" + FormatterTest.LINE_SEPARATOR, () -> {
-			Program.main(new String[] { "-o=hi", "8" });
-		});
-	}
+    @Test
+    void testEjemplohi8() throws Throwable {
+        WritterTest.compararStream("fibo<8>: 13 8 5 3 2 1 1 0" + FormatterTest.LINE_SEPARATOR, () -> {
+            Program.main(new String[] { "-o=hi", "8" });
+        });
+    }
 
-	@Test
-	void testEjemplovi8() throws Throwable {
-		WritterTest.compararStream("fibo<8>:" + FormatterTest.LINE_SEPARATOR + "13" + FormatterTest.LINE_SEPARATOR + "8"
-				+ FormatterTest.LINE_SEPARATOR + "5" + FormatterTest.LINE_SEPARATOR + "3" + FormatterTest.LINE_SEPARATOR
-				+ "2" + FormatterTest.LINE_SEPARATOR + "1" + FormatterTest.LINE_SEPARATOR + "1"
-				+ FormatterTest.LINE_SEPARATOR + "0" + FormatterTest.LINE_SEPARATOR, () -> {
-					Program.main(new String[] { "-o=vi", "8" });
-				});
-	}
+    @Test
+    void testEjemplovi8() throws Throwable {
+        WritterTest.compararStream("fibo<8>:" + FormatterTest.LINE_SEPARATOR + "13" + FormatterTest.LINE_SEPARATOR + "8"
+                + FormatterTest.LINE_SEPARATOR + "5" + FormatterTest.LINE_SEPARATOR + "3" + FormatterTest.LINE_SEPARATOR
+                + "2" + FormatterTest.LINE_SEPARATOR + "1" + FormatterTest.LINE_SEPARATOR + "1"
+                + FormatterTest.LINE_SEPARATOR + "0" + FormatterTest.LINE_SEPARATOR, () -> {
+                    Program.main(new String[] { "-o=vi", "8" });
+                });
+    }
 
-	@Test
-	void testEjemploxy8() throws Throwable {
-		WritterTest.compararStream("Opciones no validas." + FormatterTest.LINE_SEPARATOR, () -> {
-			Program.main(new String[] { "-o=xy", "8" });
-		});
-	}
+    @Test
+    void testEjemploxy8() throws Throwable {
+        WritterTest.compararStream("Opciones no validas." + FormatterTest.LINE_SEPARATOR, () -> {
+            Program.main(new String[] { "-o=xy", "8" });
+        });
+    }
 
-	// Fibo
-	@Test
-	void testEjemplo8() throws Throwable {
-		WritterTest.compararStream("fibo<8>: 0 1 1 2 3 5 8 13" + FormatterTest.LINE_SEPARATOR, () -> {
-			Program.main(new String[] { "8" });
-		});
-	}
+    // Fibo
+    @Test
+    void testEjemplo8() throws Throwable {
+        WritterTest.compararStream("fibo<8>: 0 1 1 2 3 5 8 13" + FormatterTest.LINE_SEPARATOR, () -> {
+            Program.main(new String[] { "8" });
+        });
+    }
 
-	@Test
-	void testEjemplo5() throws Throwable {
-		WritterTest.compararStream("fibo<5>: 0 1 1 2 3" + FormatterTest.LINE_SEPARATOR, () -> {
-			Program.main(new String[] { "5" });
-		});
-	}
+    @Test
+    void testEjemplo5() throws Throwable {
+        WritterTest.compararStream("fibo<5>: 0 1 1 2 3" + FormatterTest.LINE_SEPARATOR, () -> {
+            Program.main(new String[] { "5" });
+        });
+    }
 
-	@Test
-	void testConsolaHorizontalDirecta() throws Throwable {
-		compararSerieConsola(true, true);
-	}
+    @Test
+    void testConsolaHorizontalDirecta() throws Throwable {
+        compararSerieConsola(true, true);
+    }
 
-	@Test
-	void testConsolaHorizontalInversa() throws Throwable {
-		compararSerieConsola(true, false);
-	}
+    @Test
+    void testConsolaHorizontalInversa() throws Throwable {
+        compararSerieConsola(true, false);
+    }
 
-	@Test
-	void testConsolaVerticalDirecta() throws Throwable {
-		compararSerieConsola(false, true);
-	}
+    @Test
+    void testConsolaVerticalDirecta() throws Throwable {
+        compararSerieConsola(false, true);
+    }
 
-	@Test
-	void testConsolaVerticalInversa() throws Throwable {
-		compararSerieConsola(false, false);
-	}
+    @Test
+    void testConsolaVerticalInversa() throws Throwable {
+        compararSerieConsola(false, false);
+    }
 
-	private void compararSerieConsola(boolean horizontal, boolean directa) throws Throwable {
-		String[] args = new String[2];
-		args[0] = "-o=";
-		if (horizontal) {
-			args[0] += "h";
-		} else {
-			args[0] += "v";
-		}
-		if (directa) {
-			args[0] += "d";
-		} else {
-			args[0] += "i";
-		}
-		String esperado;
-		for (int i = 0; i <= FiboTest.MAX_VALIDO; i++) {
-			esperado = "fibo<" + Integer.toString(i) + ">:" + FormatterTest.getSerieFibo(i, horizontal, directa)
-					+ FormatterTest.LINE_SEPARATOR;
-			args[1] = Integer.toString(i);
-			WritterTest.compararStream(esperado, () -> {
-				Program.main(args);
-			});
-		}
-	}
+    private void compararSerieConsola(boolean horizontal, boolean directa) throws Throwable {
+        String[] args = new String[2];
+        args[0] = "-o=";
+        if (horizontal) {
+            args[0] += "h";
+        } else {
+            args[0] += "v";
+        }
+        if (directa) {
+            args[0] += "d";
+        } else {
+            args[0] += "i";
+        }
+        String esperado;
+        for (int i = 0; i <= FiboTest.MAX_VALIDO; i++) {
+            esperado = "fibo<" + Integer.toString(i) + ">:" + FormatterTest.getSerieFibo(i, horizontal, directa)
+                    + FormatterTest.LINE_SEPARATOR;
+            args[1] = Integer.toString(i);
+            WritterTest.compararStream(esperado, () -> {
+                Program.main(args);
+            });
+        }
+    }
 
 }
--- Fibo3/src/test/java/ar/edu/untref/dyasc/WritterTest.java
+++ Fibo3/src/test/java/ar/edu/untref/dyasc/WritterTest.java
@@ -16,56 +16,56 @@ import ar.edu.untref.dyasc.parser.OpcionInvalidaException;
 
 public class WritterTest {
 
-	private Writter writter;
+    private Writter writter;
 
-	@BeforeEach
-	public void setUpBeforeEach() throws Exception {
-		writter = new Writter();
-	}
+    @BeforeEach
+    public void setUpBeforeEach() throws Exception {
+        writter = new Writter();
+    }
 
-	@Test
-	void testStream() throws Throwable {
-		String mensaje = "fibo<5>:" + FormatterTest.LINE_SEPARATOR + "0" + FormatterTest.LINE_SEPARATOR + "1"
-				+ FormatterTest.LINE_SEPARATOR + "1" + FormatterTest.LINE_SEPARATOR + "2" + FormatterTest.LINE_SEPARATOR
-				+ "3";
-		compararStream(mensaje + FormatterTest.LINE_SEPARATOR, () -> {
-			writter.escribirResultado(null, mensaje);
-		});
-	}
+    @Test
+    void testStream() throws Throwable {
+        String mensaje = "fibo<5>:" + FormatterTest.LINE_SEPARATOR + "0" + FormatterTest.LINE_SEPARATOR + "1"
+                + FormatterTest.LINE_SEPARATOR + "1" + FormatterTest.LINE_SEPARATOR + "2" + FormatterTest.LINE_SEPARATOR
+                + "3";
+        compararStream(mensaje + FormatterTest.LINE_SEPARATOR, () -> {
+            writter.escribirResultado(null, mensaje);
+        });
+    }
 
-	@Test
-	void testArchivo() throws OpcionInvalidaException, IOException {
-		String archivo = "salida.txt";
-		String esperado = "fibo<5>:\\n0\\n1\\n1\\n2\\n3\\n";
-		writter.escribirResultado(archivo, esperado);
-		compararArchivo(archivo, esperado);
-	}
+    @Test
+    void testArchivo() throws OpcionInvalidaException, IOException {
+        String archivo = "salida.txt";
+        String esperado = "fibo<5>:\\n0\\n1\\n1\\n2\\n3\\n";
+        writter.escribirResultado(archivo, esperado);
+        compararArchivo(archivo, esperado);
+    }
 
-	@Test
-	void testMensajeArchivo() throws Throwable {
-		String archivo = "salida.txt";
-		String esperado = "fibo<5>:\\n0\\n1\\n1\\n2\\n3\\n";
-		compararStream("fibo<5> guardado en salida.txt" + FormatterTest.LINE_SEPARATOR, () -> {
-			writter.escribirResultado(archivo, esperado);
-		});
-	}
+    @Test
+    void testMensajeArchivo() throws Throwable {
+        String archivo = "salida.txt";
+        String esperado = "fibo<5>:\\n0\\n1\\n1\\n2\\n3\\n";
+        compararStream("fibo<5> guardado en salida.txt" + FormatterTest.LINE_SEPARATOR, () -> {
+            writter.escribirResultado(archivo, esperado);
+        });
+    }
 
-	public static void compararStream(String esperado, Executable executable) throws Throwable {
-		ByteArrayOutputStream byteArrayOutputStream;
-		byteArrayOutputStream = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(byteArrayOutputStream));
-		executable.execute();
-		Assert.assertEquals(esperado, byteArrayOutputStream.toString());
-	}
+    public static void compararStream(String esperado, Executable executable) throws Throwable {
+        ByteArrayOutputStream byteArrayOutputStream;
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        executable.execute();
+        Assert.assertEquals(esperado, byteArrayOutputStream.toString());
+    }
 
-	public static void compararArchivo(String archivo, String esperado) throws IOException {
-		String obtenido = readFile(archivo, Charset.defaultCharset());
-		Assert.assertEquals(esperado, obtenido);
-	}
+    public static void compararArchivo(String archivo, String esperado) throws IOException {
+        String obtenido = readFile(archivo, Charset.defaultCharset());
+        Assert.assertEquals(esperado, obtenido);
+    }
 
-	private static String readFile(String path, Charset encoding) throws IOException {
-		byte[] encoded = Files.readAllBytes(Paths.get(path));
-		return new String(encoded, encoding);
-	}
+    private static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
 
 }
